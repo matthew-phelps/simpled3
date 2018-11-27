39cffbcbc0b9ddd4bec681d414036d293349eaaf
Matthew Phelps <ma.phelps@gmail.com>
2018-11-27 10:19:32 +0100

    m

--- inst/htmlwidgets/simpled3.js
+++ inst/htmlwidgets/simpled3.js
@@ -10,33 +10,26 @@ HTMLWidgets.widget({
 
     var initialized = false;
     var margin = ({top:10, right:10, bottom:40, left:60});
-    var width = width - margin.left - margin.right
-    var height = height -margin.top - margin.bottom
+    var width = width - margin.left - margin.right;
+    var height = height -margin.top - margin.bottom;
 
     var barPadding = 0.2;
-    var colors = ['#bd6916', '#166abd ']
+    var colors = ['#bd6916', '#166abd '];
 
     var tLong = 450;
     var tShort = 200;
     var cRadius = 7;
 
-    var scaleX = d3.scaleLinear()
-      .domain(d3.extent(data, d => d.year))
-      .range([0, width])
-
-    var scaleY = d3.scaleLinear()
-      .domain([0, maxY])
-      .range([height, 0]);
 
     var scaleColors = d3.scaleOrdinal()
-        .range(colors)
+        .range(colors);
 
     // Initial axis
     var yAxis = topG.append('g')
-      .attr("class", "y axis")
+      .attr("class", "y axis");
 
     var xAxis = topG.append('g')
-        .attr("class", "x axis")
+        .attr("class", "x axis");
 
 
 
@@ -45,7 +38,7 @@ HTMLWidgets.widget({
       .attr("height", height + margin.top + margin.bottom);
 
     var topG = svg.append('g')
-      .attr('transform', 'translate(' + margin.left + ',' + margin.top +')')
+      .attr('transform', 'translate(' + margin.left + ',' + margin.top +')');
 
     var chartArea = topG.append("g");
 
@@ -62,58 +55,56 @@ HTMLWidgets.widget({
       .attr("x", 0 - height / 2)
       .attr("y", 0 - margin.left + 20)
       .attr("class", "y axisTitle")
-      .text("Total")
+      .text("Total");
 
+    // Tooltip
 
+    var div = topG.append("div")
+        .attr('class', 'tooltip')
+        .style('opacity', 0)
+        .attr("position", "absolute")
+        .attr("text-align", "center")
+        .attr("width"," 60px")
+        .attr("height", "28px")
+        /*.attr("padding", "2px")				*/
+        /*.attr("font", "12px sans-serif")*/
+        .attr("fill", "lightsteelblue");
 
     return {
 
       renderValue: function(x) {
 
+        var data = HTMLWidgets.dataframeToD3(x.data);
+        var varName = data[0].variable;
+        var grouping1Names = data.map(d => d.year);
+
+        // Scales
+        var maxY = d3.max(data, d=> Math.max(d.female, d.male));
+        var scaleX = d3.scaleLinear()
+          .domain(d3.extent(data, d => d.year))
+          .range([0, width]);
+
+        var scaleY = d3.scaleLinear()
+          .domain([0, maxY])
+          .range([height, 0]);
+
+        xAxis.call(d3.axisBottom(scaleX)
+            .tickFormat(d3.format("")))
+            .attr("transform", "translate(" + 0 + "," + height + ')');
+
+        // Line generators
+        var valueLine1 = d3.line()
+        .x(d => scaleX(d.year))
+        .y(d => scaleY(d.female));
+
+        var valueLine2 = d3.line()
+        .x(d => scaleX(d.year))
+        .y(d => scaleY(d.male));
+
+        // Add initial line paths
         if (!initialized){
-          initialized = true;
+            initialized = true;
 
-
-
-          var data = HTMLWidgets.dataframeToD3(x.data)
-
-
-
-
-
-
-
-
-
-          // Initial scale
-
-          var maxY = d3.max(data, d=> Math.max(d.female, d.male))
-
-
-          xAxis.call(d3.axisBottom(scaleX)
-              .tickFormat(d3.format("")))
-              .attr("transform", "translate(" + 0 + "," + height + ')')
-
-
-
-
-
-          grouping1Names = data.map(d => d.year);
-
-
-          // Scales used in updates
-
-
-          // Line generators
-          var valueLine1 = d3.line()
-          .x(d => scaleX(d.year))
-          .y(d => scaleY(d.female))
-
-          var valueLine2 = d3.line()
-          .x(d => scaleX(d.year))
-          .y(d => scaleY(d.male))
-
-          // Add initial line paths
            var line1 = chartArea
             .append("path")
             .datum(data) // use dataum() because appending to single svg element
@@ -154,98 +145,58 @@ HTMLWidgets.widget({
               .attr("r", cRadius)
               .attr("fill", colors[1]);
 
-
-          var div = topG.append("div")
-              .attr('class', 'tooltip')
-              .style('opacity', 0)
-              .attr("position", "absolute")
-              .attr("text-align", "center")
-              .attr("width"," 60px")
-              .attr("height", "28px")
-              /*.attr("padding", "2px")				*/
-              /*.attr("font", "12px sans-serif")*/
-              .attr("fill", "lightsteelblue")
-              /*.attr("border", "0px")
-              .attr("border-radius"," 8px")
-          */
         }
-          // Only run on update data
-
-            // Reshape data
-            var newData = HTMLWidgets.dataframeToD3(x.data)
 
 
-            var maxY = d3.max(newData, d=> Math.max(d.female, d.male))
-            var varName = newData[0].variable
+         // Create the paths for each series of data
+        chartArea
+          .select(".female")
+          .transition()
+          .duration(tLong)
+          .attr("d", valueLine1(data));
 
+        chartArea
+          .select(".male")
+          .transition()
+          .duration(tLong)
+          .attr("d", valueLine2(data));
 
-            // Scales used in updates
-            var scaleY = d3.scaleLinear()
-              .domain([0, maxY])
-              .range([height, 0]);
+        // Update circles
+        var dotFemale = chartArea.selectAll(".dotfemale")
+          .data(data);
 
-            var scaleX = d3.scaleLinear()
-            .domain(d3.extent(newData, d => d.year))
-            .range([0, width])
+          dotFemale.transition()
+          .duration(tLong)
+          .attr("cx", d => scaleX(d.year))
+          .attr("cy", d => scaleY(d.female));
 
-            // Line generators
-            var valueLine1 = d3.line()
-            .x(d => scaleX(d.year))
-            .y(d => scaleY(d.female))
-
-            var valueLine2 = d3.line()
-            .x(d => scaleX(d.year))
-            .y(d => scaleY(d.male))
-
-             // Create the paths for each series of data
-            chartArea
-              .select(".female")
-              .transition()
-              .duration(tLong)
-              .attr("d", valueLine1(newData))
-
-            chartArea
-              .select(".male")
-              .transition()
-              .duration(tLong)
-              .attr("d", valueLine2(newData))
-
-            // Update circles
-            var dotFemale = chartArea.selectAll(".dotfemale")
-              .data(newData)
-
-              dotFemale.transition()
-              .duration(tLong)
-              .attr("cx", d => scaleX(d.year))
-              .attr("cy", d => scaleY(d.female))
-
-              dotFemale.on("mouseover", function(d){
-                                    div.transition()
-                                    .duration(tShort)
-                                    .style('opacity', .9);
-                                    div.html(d)
-                                      .style("left", d3.event.pageX + "px")
-                                      .style("top", (d3.event.pageY - 28) + "px");
-              })
+          dotFemale.on("mouseover", function(d){
+                                div.transition()
+                                .duration(tShort)
+                                .style('opacity', 0.9);
+                                div.html(d)
+                                  .style("left", d3.event.pageX + "px")
+                                  .style("top", (d3.event.pageY - 28) + "px");
+          });
 
 
             chartArea.selectAll(".dotmale")
-              .data(newData)
+              .data(sdata)
               .transition()
               .duration(tLong)
               .attr("cx", d => scaleX(d.year))
-              .attr("cy", d => scaleY(d.male))
+              .attr("cy", d => scaleY(d.male));
 
 
             // Udpate axes
             yAxis.transition()
               .duration(tLong)
-              .call(d3.axisLeft(scaleY))
+              .call(d3.axisLeft(scaleY));
 
             xAxis.transition()
               .duration(tLong)
               .call(d3.axisBottom(scaleX)
-                      .tickFormat(d3.format("")))
+                      .tickFormat(d3.format("")));
 
             // Update axis titles
             topG.select(".y.axisTitle")
