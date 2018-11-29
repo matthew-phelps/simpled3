3729334735b3474a11d9bb5fe28430fb07476d4d
Matthew Phelps <ma.phelps@gmail.com>
2018-11-29 15:04:49 +0100

    m

--- inst/htmlwidgets/simpleD3Line.js
+++ inst/htmlwidgets/simpleD3Line.js
@@ -7,14 +7,15 @@ HTMLWidgets.widget({
   factory: function(el, width, height) {
     var chartExists = false;
     var margin = ({top:10, right:20, bottom:40, left:60});
+    rectPadding = 0.2;
     return {
 
       renderValue: function(x) {
         if(!chartExists){
           chartExists = true;
-          drawLineChart(x, width, height, el, margin);
+          drawLineChart(x, width, height, el, margin, rectPadding);
         } else {
-          updateLineChart(x, this.dim.width, this.dim.height, el, margin);
+          updateLineChart(x, this.dim.width, this.dim.height, el, margin, rectPadding);
         }
         this.x = x;
 
@@ -22,7 +23,7 @@ HTMLWidgets.widget({
 
       resize: function(width, height) {
 
-        resizeLineChart(this.x, width, height, el, margin);
+        resizeLineChart(this.x, width, height, el, margin, rectPadding);
           var dim = {
                 width: width - margin.left - margin.right,
                 height: height - margin.top - margin.bottom
