238cc6ea0604b83632d212d24749f19354f0718f
Matthew Phelps <ma.phelps@gmail.com>
2018-12-03 10:47:42 +0100

    Needed to define circle positions with mouseover events in orde to ensure circles are place correctly is mouseover function is active during window resize

--- inst/htmlwidgets/lib/lineChartDefs.js
+++ inst/htmlwidgets/lib/lineChartDefs.js
@@ -473,9 +473,6 @@ var scaleXRects = d3.scaleBand()
         .ease(d3.easeLinear)
         .duration("200")
         .attr("r", bigRadius);
-    tooltip.transition()
-    .duration(tShort)
-    .style('opacity', 0.9);
     
   }
 
@@ -488,12 +485,7 @@ var scaleXRects = d3.scaleBand()
       .attr("cy", d => scaleY(d.male));
 
 
-    tooltip.html(
-      varName + " i " +"<b>" + d.year + "</b>" + "<br/><br/>" +
-       Object.keys(d)[1] + ": <b>" + d.female + "</b><br/>" +
-       Object.keys(d)[2] + ": <b>" + d.male + "</b>")
-    .style("left", d3.mouse(this)[0] + "px")
-    .style("top", (d3.mouse(this)[1] + 28) + "px");
+ 
   }
 
   function hideTooltip(d) {
@@ -502,9 +494,7 @@ var scaleXRects = d3.scaleBand()
         .ease(d3.easeLinear)
         .duration("200")
         .attr("r", cRadius);
-    tooltip.transition()
-    .duration(tShort)
-    .style('opacity', 0);
+ 
   }
 
 
