7d6483af6dd0f7e3ddfe84c4939cf5a4edf65916
Matthew Phelps <ma.phelps@gmail.com>
2018-12-10 09:36:10 +0100

    cont

--- inst/htmlwidgets/lib/legend.js
+++ inst/htmlwidgets/lib/legend.js
@@ -7,12 +7,16 @@ function drawLegend(x, width, height, el, margin) {
     width: width - margin.left - margin.right,
     height: height - margin.top - margin.bottom
   };
-  
+
 // ADD LEGEND BOXES
   svgLegend = d3.select(el)
     .append('svg')
     .attr('width', dim.width + margin.left + margin.right)
     .attr('height', dim.height + margin.top + margin.bottom);
+
+  legendWrapper = svgLegend.append('g')
+  	.attr("class", "legendWrapper")
+  	.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 
 
