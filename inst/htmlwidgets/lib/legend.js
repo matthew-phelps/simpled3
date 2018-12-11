34b9a8044adf093013bca4e097c31c71596e85d4
Matthew Phelps <ma.phelps@gmail.com>
2018-12-11 10:52:41 +0100

    male legend entry

--- inst/htmlwidgets/lib/legend.js
+++ inst/htmlwidgets/lib/legend.js
@@ -8,7 +8,7 @@ function drawLegend(fromR, width, height, el, margin) {
     height: height - margin.top - margin.bottom
   };
 
-  var rectSize = 25; //dimension of colored square
+  var rectSize = 20; //dimension of colored square
   var padding = 10; 
   var entryWidth = 100; // width of square and text combined
 // ADD LEGEND BOXES
@@ -35,8 +35,15 @@ function drawLegend(fromR, width, height, el, margin) {
 	 	.attr('transform', 'translate(' + (rectSize + padding) +',' + rectSize/2 + ')')
 	 	.attr('alignment-baseline', 'middle') // verticle alignment?
 	 	.text(fromR.data.sex[0]);
+
+	 legend.append("rect")
+  		.attr("width", rectSize)
+	  	.attr("height", rectSize)
+	  	.attr('transform', 'translate(' + entryWidth + ',' + 0 + ')')
+	  	.style("fill", fromR.data.color[1]);
+	 legend.append("text")
+	 	.attr('transform', 'translate(' + (entryWidth + rectSize + padding) +',' + rectSize/2 + ')')
+	 	.attr('alignment-baseline', 'middle') // verticle alignment?
+	 	.text(fromR.data.sex[1]);
 	  	
-
-
-
 }
