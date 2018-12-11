f11944d1528f27fa431b568bc6667b9587c0d62a
Matthew Phelps <ma.phelps@gmail.com>
2018-12-11 11:02:12 +0100

    adding resizng - attempt 1

--- inst/htmlwidgets/lib/legend.js
+++ inst/htmlwidgets/lib/legend.js
@@ -14,6 +14,7 @@ function drawLegend(fromR, width, height, el, margin) {
 // ADD LEGEND BOXES
  	var svgLegend = d3.select(el)
     	.append('svg')
+    	.attr('class', 'svgLegend')
 	    .attr('width', dim.width + margin.left + margin.right)
 	    .attr('height', dim.height + margin.top + margin.bottom);
 
@@ -22,28 +23,61 @@ function drawLegend(fromR, width, height, el, margin) {
   		.attr("transform" , 'translate(' + margin.left + ',' + margin.top + ')');
   		
 
-  	var legend = legendWrapper.append('g')
+  	var legendFemale = legendWrapper.append('g')
+  		.attr('class', 'legend female')
   		.attr("transform" , 'translate(' + (dim.width/2 - entryWidth) + ',' + 0 + ')');	
+  	
+  	var legendMale = legendWrapper.append('g')
+  		.attr('class', 'legend male')
+  		.attr("transform" , 'translate(' + (dim.width/2) + ',' + 0 + ')');	
   	
 
 
-	legend.append("rect")
+	legendFemale.append("rect")
   		.attr("width", rectSize)
 	  	.attr("height", rectSize)
 	  	.style("fill", fromR.data.color[0]);
-	 legend.append("text")
+	 legendFemale.append("text")
 	 	.attr('transform', 'translate(' + (rectSize + padding) +',' + rectSize/2 + ')')
 	 	.attr('alignment-baseline', 'middle') // verticle alignment?
 	 	.text(fromR.data.sex[0]);
 
-	 legend.append("rect")
+	 legendMale.append("rect")
   		.attr("width", rectSize)
 	  	.attr("height", rectSize)
-	  	.attr('transform', 'translate(' + entryWidth + ',' + 0 + ')')
 	  	.style("fill", fromR.data.color[1]);
-	 legend.append("text")
-	 	.attr('transform', 'translate(' + (entryWidth + rectSize + padding) +',' + rectSize/2 + ')')
+	 legendMale.append("text")
+	 	.attr('transform', 'translate(' + (rectSize + padding) +',' + rectSize/2 + ')')
 	 	.attr('alignment-baseline', 'middle') // verticle alignment?
 	 	.text(fromR.data.sex[1]);
+	  	
+}
+
+
+
+function resizeLegend(fromR, width, height, el, margin) {
+ var dim = {
+    width: width - margin.left - margin.right,
+    height: height - margin.top - margin.bottom
+  };
+
+  var rectSize = 20; //dimension of colored square
+  var padding = 10; 
+  var entryWidth = 100; // width of square and text combined
+// ADD LEGEND BOXES
+ 	var svgLegend = d3.select('.svgLegend')
+	    .attr('width', dim.width + margin.left + margin.right)
+	    .attr('height', dim.height + margin.top + margin.bottom);
+
+	var legendWrapper = svgLegend.select('.legendWrapper')
+  		.attr("transform" , 'translate(' + margin.left + ',' + margin.top + ')');
+  		
+  	legendWrapper.select('.legend.female')
+  		.attr("transform" , 'translate(' + (dim.width/2 - entryWidth) + ',' + 0 + ')');	
+  	
+  	legendWrapper.select('.legend.male')
+  		.attr("transform" , 'translate(' + (dim.width/2) + ',' + 0 + ')');	
+
+
 	  	
 }
