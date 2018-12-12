f6e2db18364652c05ef1155ee7566b8fd002bbae
Matthew Phelps <ma.phelps@gmail.com>
2018-12-12 08:59:58 +0100

    stop redrawing legend

--- inst/htmlwidgets/simpleD3Legend.js
+++ inst/htmlwidgets/simpleD3Legend.js
@@ -7,13 +7,16 @@ HTMLWidgets.widget({
   factory: function(el, width, height) {
 
     var margin = ({top:10, right:10, bottom:10, left:60});
-
+    var chartExists = false;
 
     return {
 
       renderValue: function(x) {
-
-        drawLegend(x, width, height, el, margin);
+        if(!chartExists){
+          chartExists = true;
+          drawLegend(x, width, height, el, margin);
+        }
+        
       },
 
       resize: function(width, height) {
