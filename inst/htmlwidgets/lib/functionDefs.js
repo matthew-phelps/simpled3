556feaa7308b403b2769f50118b94b1997dc13f9
Matthew Phelps <ma.phelps@gmail.com>
2019-01-11 14:31:13 +0100

    cont

--- inst/htmlwidgets/lib/functionDefs.js
+++ inst/htmlwidgets/lib/functionDefs.js
@@ -185,7 +185,7 @@ function drawBarChart(inData, width, height, el, margin, colors, barPadding, tLo
 
 
   /// ADD LEGEND
-  drawLegend(inData, dim, legendHeight);
+  drawLegend(inData, dim, margin, legendHeight);
 
   // Tooltip table setup
   var cellSvgWidth = "20%";
