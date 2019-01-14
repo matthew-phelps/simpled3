38e559810d9151f7eb27e3e51311cbb3dc81d391
Matthew Phelps <ma.phelps@gmail.com>
2019-01-14 12:48:37 +0100

    cont

--- inst/htmlwidgets/lib/legend.js
+++ inst/htmlwidgets/lib/legend.js
@@ -3,7 +3,7 @@
 //////////////////////////////////////////////////////////////////////////////
 
 function drawLegend(topG, inData, dim, margin, legendHeight, wrapperName, svgName) {
- 
+
 
   var rectSize = 20; //dimension of colored square
   var padding = 10; 
@@ -13,57 +13,59 @@ function drawLegend(topG, inData, dim, margin, legendHeight, wrapperName, svgNam
 
 
 // ADD LEGEND BOXES
- 	
-var legendWrapper = topG.append('g')
-      .attr("class", wrapperName)
-      .attr("transform" , 'translate(' + (dim.width / 2 - legendWidth / 2) + ',' + (dim.height) + ')');
 
-  	// Wrappers for each gender	
-  	var legendMale = legendWrapper.append('g')
-  		.attr('class', 'legend male')
-  		.attr("transform" , 'translate(' + (0) + ',' + 0 + ')');	
-  	
-    var legendFemale = legendWrapper.append('g')
+  var legendWrapper = topG.append('g')
+  .attr("class", wrapperName)
+  .attr("transform" , 'translate(' + (dim.width / 2 - legendWidth / 2) + ',' + (dim.height) + ')');
+
+    	// Wrappers for each gender	
+    	var legendMale = legendWrapper.append('g')
+      .attr('class', 'legend male')
+      .attr("transform" , 'translate(' + (0) + ',' + 0 + ')');	
+
+      var legendFemale = legendWrapper.append('g')
       .attr('class', 'legend female')
       .attr("transform" , 'translate(' + (legendWidth/2 + 2*padding) + ',' + 0 + ')');  
-    
+      
 
-  // Append rect and text for each gender
-	 legendMale.append("rect")
-  		.attr("width", rectSize)
-	  	.attr("height", rectSize)
-	  	.style("fill", inData.legend.colors[1]);
+    // Append rect and text for each gender
+    legendMale.append("rect")
+    .attr("width", rectSize)
+    .attr("height", rectSize)
+    .style("fill", inData.legend.colors[1]);
 
-	 legendMale.append("text")
-	 	.attr('transform', 'translate(' + (rectSize + padding) +',' + rectSize/2 + ')')
-	 	.attr('alignment-baseline', 'middle') // verticle alignment?
-	 	.text(inData.legend.legendData.sex[1]);
-
-	  	
-	legendFemale.append("rect")
-  		.attr("width", rectSize)
-	  	.attr("height", rectSize)
-	  	.style("fill", inData.legend.colors[0]);
-	 legendFemale.append("text")
-	 	.attr('transform', 'translate(' + (rectSize + padding) +',' + rectSize/2 + ')')
-	 	.attr('alignment-baseline', 'middle') // verticle alignment?
-	 	.text(inData.legend.legendData.sex[0]);
+    legendMale.append("text")
+    .attr('transform', 'translate(' + (rectSize + padding) +',' + rectSize/2 + ')')
+  	 	.attr('alignment-baseline', 'middle') // verticle alignment?
+  	 	.text(inData.legend.legendData.sex[1]);
 
 
-/// ADD DOWNLOAD BUTTON
-var downloadButtonWrapper = topG.append('g')
+     legendFemale.append("rect")
+     .attr("width", rectSize)
+     .attr("height", rectSize)
+     .style("fill", inData.legend.colors[0]);
+     legendFemale.append("text")
+     .attr('transform', 'translate(' + (rectSize + padding) +',' + rectSize/2 + ')')
+  	 	.attr('alignment-baseline', 'middle') // verticle alignment?
+  	 	.text(inData.legend.legendData.sex[0]);
+
+
+  /// ADD DOWNLOAD BUTTON
+  var downloadButtonWrapper = topG.append('g')
   .attr("class", wrapperName + "downloadButton")
   .attr("transform" , 'translate(' + (dim.width - downloadButtonWidth) + ',' + (dim.height) + ')');
 
-var downloadButton = downloadButtonWrapper
+  var downloadButton = downloadButtonWrapper
   .append('rect')
   .attr('class', 'download button')
   .attr("width", downloadButtonWidth)
   .attr("height", rectSize * 1.2)
   .style("fill", "white")
-  .style("stroke", "black")
+  .style("stroke", "black");
+
+  downloadButtonWrapper
   .text("Download")
-    .attr("fill", "black");
+  .attr("fill", "black");
 
 
 
@@ -72,16 +74,16 @@ var downloadButton = downloadButtonWrapper
 
 
 function resizeLegend(dim, wrapperName) {
- 
+
   var rectSize = 20; //dimension of colored square
   var padding = 10; 
   var entryWidth = 100; // width of square and text combined
   var legendWidth = rectSize + 2*padding + entryWidth;
 
 // ADD LEGEND BOXES
-	var legendWrapper = d3.select('.' + wrapperName)
-  		.attr("transform" , 'translate(' + (dim.width / 2 - legendWidth / 2) + ',' + (dim.height) + ')');
-  		
- 
-	  	
+var legendWrapper = d3.select('.' + wrapperName)
+.attr("transform" , 'translate(' + (dim.width / 2 - legendWidth / 2) + ',' + (dim.height) + ')');
+
+
+
 }
