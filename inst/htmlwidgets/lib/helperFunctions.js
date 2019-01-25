5c96dfec286140913880acae2a22bf4f26cee25a
Matthew Phelps <ma.phelps@gmail.com>
2019-01-25 10:44:16 +0100

    cont

--- inst/htmlwidgets/lib/helperFunctions.js
+++ inst/htmlwidgets/lib/helperFunctions.js
@@ -14,7 +14,7 @@ var insertLinebreaks = function (d) {
 
 
 
-function showTooltip(, thead, maleCell, femaleCell) {
+function showTooltip(d, thead, maleCell, femaleCell) {
     thead.text(groupingName + ": " + d.key);
     maleCell.text(d.values[0].sex + ": " + numberFormat(d.values[0].value));
     femaleCell.text(d.values[1].sex + ": " + numberFormat(d.values[1].value));
