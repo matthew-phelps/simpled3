125db21f39e52338e26460e546c721f78f1c849b
Matthew Phelps <ma.phelps@gmail.com>
2019-02-04 14:26:27 +0100

    cont

--- inst/htmlwidgets/lib/functionDefs.js
+++ inst/htmlwidgets/lib/functionDefs.js
@@ -354,10 +354,10 @@ var chartType = "Bar";
 
   // Remove any bar-groups not present in incoming data
   barGroupWithData.exit()
-   /* .transition()
+    .transition()
     .duration(tShort)
     .ease(d3.easeLinear)
-    .style('opacity', 0)*/
+    .style('opacity', 0)
     .remove();
 
 
