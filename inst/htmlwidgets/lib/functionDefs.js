59bd10ad79fdd7ad1767642562be7b417eb5aa72
Matthew Phelps <ma.phelps@gmail.com>
2019-03-20 11:37:32 +0100

    Trying again wrap fn

--- inst/htmlwidgets/lib/functionDefs.js
+++ inst/htmlwidgets/lib/functionDefs.js
@@ -202,7 +202,8 @@ function drawBarChart(
     .duration(tLong)
     .style("opacity", 1)
     .text(groupingName)
-    .style("text-anchor", "middle");
+    .style("text-anchor", "middle")
+    .call(wrap, 20);
 
   /// ADD LEGEND
   var wrapperName = "legendWrapper" + chartType;
@@ -268,6 +269,32 @@ function drawBarChart(
       .style("opacity", 0)
       .remove();
   }
+
+function wrap(text, width) {
+  text.each(function() {
+    var text = d3.select(this),
+        words = text.text().split(/\s+/).reverse(),
+        word,
+        line = [],
+        lineNumber = 0,
+        lineHeight = 1.1, // ems
+        y = text.attr("y"),
+        dy = parseFloat(text.attr("dy")),
+        tspan = text.text(null).append("tspan").attr("x", 0).attr("y", y).attr("dy", dy + "em");
+    while (word = words.pop()) {
+      line.push(word);
+      tspan.text(line.join(" "));
+      if (tspan.node().getComputedTextLength() > width) {
+        line.pop();
+        tspan.text(line.join(" "));
+        line = [word];
+        tspan = text.append("tspan").attr("x", 0).attr("y", y).attr("dy", ++lineNumber * lineHeight + dy + "em").text(word);
+      }
+    }
+  });
+}
+
+
 
 }
 
