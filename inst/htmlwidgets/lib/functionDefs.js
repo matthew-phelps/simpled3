e8fa1f57a52c4260719598304de2de48d3e83240
Matthew Phelps <ma.phelps@gmail.com>
2019-03-20 11:55:20 +0100

    TRying to pass y axis text to wrap function

--- inst/htmlwidgets/lib/functionDefs.js
+++ inst/htmlwidgets/lib/functionDefs.js
@@ -194,6 +194,7 @@ function drawBarChart(
     .transition()
     .duration(tLong)
     .text(varName)
+    .selectAll("text")
     .style("text-anchor", "middle")
     .call(wrap, 20);
 
@@ -204,7 +205,6 @@ function drawBarChart(
     .style("opacity", 1)
     .text(groupingName)
     .style("text-anchor", "middle");
-
 
   /// ADD LEGEND
   var wrapperName = "legendWrapper" + chartType;
@@ -271,36 +271,43 @@ function drawBarChart(
       .remove();
   }
 
-function wrap(text, width) {
-  text.each(function() {
-    var text = d3.select(this),
-        words = text.text().split(/\s+/).reverse(),
+  function wrap(text, width) {
+    text.each(function() {
+      var text = d3.select(this),
+        words = text
+          .text()
+          .split(/\s+/)
+          .reverse(),
         word,
         line = [],
         lineNumber = 0,
         lineHeight = 1.1, // ems
         y = text.attr("y"),
         dy = parseFloat(text.attr("dy")),
-        tspan = text.text(null).append("tspan").attr("x", 0).attr("y", y).attr("dy", dy + "em");
-    while (word = words.pop()) {
-      line.push(word);
-      tspan.text(line.join(" "));
-      if (tspan.node().getComputedTextLength() > width) {
-        line.pop();
+        tspan = text
+          .text(null)
+          .append("tspan")
+          .attr("x", 0)
+          .attr("y", y)
+          .attr("dy", dy + "em");
+      while ((word = words.pop())) {
+        line.push(word);
         tspan.text(line.join(" "));
-        line = [word];
-        tspan = text.append("tspan").attr("x", 0).attr("y", y).attr("dy", ++lineNumber * lineHeight + dy + "em").text(word);
+        if (tspan.node().getComputedTextLength() > width) {
+          line.pop();
+          tspan.text(line.join(" "));
+          line = [word];
+          tspan = text
+            .append("tspan")
+            .attr("x", 0)
+            .attr("y", y)
+            .attr("dy", ++lineNumber * lineHeight + dy + "em")
+            .text(word);
+        }
       }
-    }
-  });
+    });
+  }
 }
-
-
-
-}
-
-
-
 
 //////////////////////////////////////////////////////////////////////////////
 //////////////////    UPDATE     /////////////////////////////////////////////
