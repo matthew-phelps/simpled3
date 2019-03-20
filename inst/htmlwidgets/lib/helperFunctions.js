3f9081b2f16ae4a2e217ad8d4201bd12a4bfa415
Matthew Phelps <ma.phelps@gmail.com>
2019-03-20 10:35:19 +0100

    Add d3plus-text library for hopefully easier text wrapping

--- inst/htmlwidgets/lib/helperFunctions.js
+++ inst/htmlwidgets/lib/helperFunctions.js
@@ -13,7 +13,30 @@ var insertLinebreaks = function(d) {
 
 
 
-console.log(wrap("sadfads asd ga asd g asd g asd ag", "10"))
+function wrap(text, width) {
+  text.each(function() {
+    var text = d3.select(this),
+        words = text.text().split(/\s+/).reverse(),
+        word,
+        line = [],
+        lineNumber = 0,
+        lineHeight = 1.1, // ems
+        y = text.attr("y"),
+        dy = parseFloat(text.attr("dy")),
+        tspan = text.text(null).append("tspan").attr("x", 0).attr("y", y).attr("dy", dy + "em");
+    while (word = words.pop()) {
+      line.push(word);
+      tspan.text(line.join(" "));
+      if (tspan.node().getComputedTextLength() > width) {
+        line.pop();
+        tspan.text(line.join(" "));
+        line = [word];
+        tspan = text.append("tspan").attr("x", 0).attr("y", y).attr("dy", ++lineNumber * lineHeight + dy + "em").text(word);
+      }
+    }
+  });
+}
+
 
 function scaffoldTooltip(rectSize, colors, chartType) {
   // Tooltip table setup
