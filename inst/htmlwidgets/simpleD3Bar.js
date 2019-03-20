8109037f9ca24ea9cb41545b7086a9cc63d4798e
Matthew Phelps <ma.phelps@gmail.com>
2019-03-20 14:06:48 +0100

    cont

--- inst/htmlwidgets/simpleD3Bar.js
+++ inst/htmlwidgets/simpleD3Bar.js
@@ -4,7 +4,7 @@ HTMLWidgets.widget({
   type: "output",
 
   factory: function(el, width, height) {
-    var margin = { top: 1, right: 40, bottom: 25, left: 70 };
+    var margin = { top: 1, right: 20, bottom: 25, left: 90 };
     var barPadding = 0.2;
     var tLong = 450;
     var tShort = 200;
