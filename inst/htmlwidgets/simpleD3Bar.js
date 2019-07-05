b216b9f58da3d63ad50064ba8116d17b9ac0af47
Matthew Phelps <ma.phelps@gmail.com>
2019-07-05 13:12:12 +0200

    Remove mouseover opacity change

--- inst/htmlwidgets/simpleD3Bar.js
+++ inst/htmlwidgets/simpleD3Bar.js
@@ -8,7 +8,7 @@ HTMLWidgets.widget({
     var barPadding = 0.2;
     var tLong = 450;
     var tShort = 200;
-    var mOpacity = 0.2;
+    var mOpacity = 0.0;
     var rectSize = 20; //dimension of colored square
     var legendHeight = 50;
     var titleHeight = 40;
